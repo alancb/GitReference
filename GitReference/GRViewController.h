11451da4836f3f9cfd12d10e777206d84a1724a1
Alan Barth <alancbarth@gmail.com>
2015-04-05 13:44:51 -0600

    Finishes Project

---
+++ GitReference/GRViewController.h
@@ -0,0 +1,13 @@
+//
+//  GRViewController.h
+//  GitReference
+//
+//  Created by Alan Barth on 3/31/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRViewController : UIViewController
+
+@end
---
+++ GitReference/GRViewController.m
@@ -0,0 +1,13 @@
+//
+//  GRViewController.m
+//  GitReference
+//
+//  Created by Alan Barth on 3/31/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "GRViewController.h"
+
+@implementation GRViewController
+
+@end
